49ee6c22debba8939b50a7d14eac8051ec139213
henriqueandras <henriqueandras@gmail.com>
2022-02-18 12:45:33 -0300

    Setup Django

--- AcaFeed/AcaFeed/wsgi.py
+++ AcaFeed/AcaFeed/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for AcaFeed project.
+WSGI config for acafeed project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AcaFeed.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acafeed.settings')
 
 application = get_wsgi_application()
