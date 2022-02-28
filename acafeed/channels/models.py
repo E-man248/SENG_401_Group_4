4f91b9359cf81d7af404bae71a5ce3831b3c9bd5
StayFrostea <59980526+StayFrostea@users.noreply.github.com>
2022-02-27 19:52:15 -0700

    Added the start of event sourcing
    In the models of post and channels.

--- acafeed/channels/models.py
+++ acafeed/channels/models.py
@@ -2,18 +2,28 @@ from django.db import models
 from django.utils import timezone
 from users.models import User
 from eventsourcing.application import Application
+from eventsourcing.domain import Aggregate, event
 
 
 # Create your models here.
 
 
-class Channel(models.Model):
+class Channel(models.Model, Application):
     name = models.CharField(max_length=255, unique=True)
     date_posted = models.DateTimeField(default=timezone.now)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.name)
+
+    def registerPost(self, title, content, user, tag):
+        post = Post(title, content, user, self, tag)
+        self.save(post)
+        return post.id
+
+    def get_post(self, post_id):
+        post = self.repository.get(post_id)
+        return {'title': post.title, 'content': post.content}
 
 
 class PostTag(models.Model):
@@ -23,7 +33,15 @@ class PostTag(models.Model):
         return str(self.tagName)
 
 
-class Post(models.Model):
+class Post(models.Model, Aggregate):
+    @event('Posted')
+    def __init__(self, title, content, user, channel, tag):
+        self.title = title
+        self.content = content
+        self.created_by = User(user)
+        self.posted_in = channel
+        self.tag = tag
+
     title = models.CharField(max_length=255)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
