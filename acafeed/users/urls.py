47bfe8bd5d833fc2dba2bc701a6a800aa6deea5d
henriqueandras <henriqueandras@gmail.com>
2022-03-12 21:10:40 -0700

    Added Users Routing and base html template

---
+++ acafeed/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('', views.homepage, name="homepage"),
+    path('login/', views.users_login, name="login"),
+    path('signup/', views.users_signup, name="signup"),
+]
--- acafeed/users/views.py
+++ acafeed/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def homepage(request):
+    return render(request, 'users/home-page.html')
+
+
+def users_login(request):
+    return render(request, 'users/login.html')
+
+
+def users_signup(request):
+    return render(request, 'users/signup.html')
