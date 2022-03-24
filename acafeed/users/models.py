1b5debb81830c73036943f9ab2c7be578f992950
mwpelletier <mwpellet@ucalgary.ca>
2022-03-24 11:03:46 -0600

    pub sub stuff

--- acafeed/users/models.py
+++ acafeed/users/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.utils import timezone
-from courses.models import Course
+from channels.models import *
 
 
 # Create your models here.
+
+class UserTag(models.Model):
+    tagName = models.CharField(max_length=32)
+
+    def __str__(self):
+        return str(self.tagName)
 
 
 class User(models.Model):
@@ -13,10 +19,13 @@ class User(models.Model):
     userName = models.CharField(max_length=32, unique=True)
     password = models.CharField(max_length=16)
     major = models.CharField(max_length=32)
+    tag = models.ManyToManyField(UserTag)
     school = models.CharField(max_length=255)
     date_joined = models.DateTimeField(default=timezone.now)
-    courses = models.ManyToManyField(Course, null=True)
-    blocked = models.BooleanField(default=False)
+
+    def unsubscribe(self, topic):
+        t = Topic.objects.get(topicName = topic)
+        self.topic_set.remove(t)
 
     def __str__(self):
         return str(self.userName)
