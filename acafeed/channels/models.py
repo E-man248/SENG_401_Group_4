193bc30dd080482ffda6e9d91c689f46d63bc7a0
mwpelletier <81388399+mwpelletier@users.noreply.github.com>
2022-03-24 13:37:36 -0600

    pub sub change

--- acafeed/channels/models.py
+++ acafeed/channels/models.py
@@ -8,33 +8,34 @@ class Channel(models.Model):
     name = models.CharField(max_length=255, unique=True)
     date_posted = models.DateTimeField(default=timezone.now)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    subscribers = models.ManyToManyField(User)
+
+    def subscribe(self, user):
+        self.subscribers.add(user)
 
     def __str__(self):
         return str(self.name)
 
+# class Topic(models.Model):
+#     topicName = models.CharField(max_length=255)
+#     description = models.TextField(max_length=400, blank=True)
+#     subscribers = models.ManyToManyField(User)
 
-class PostTag(models.Model):
-    tagName = models.CharField(max_length=32)
-
-    def __str__(self):
-        return str(self.tagName)
-
-class Topic(models.Model):
-    topicName = models.CharField(max_length=255)
-    description = models.TextField(max_length=400, blank=True)
-    subscribers = models.ManyToManyField(User)
-
-    def __str__(self):
-        return str(self.topicName)
+#     def __str__(self):
+#         return str(self.topicName)
 
 
 class Message(models.Model):
     messageText = models.TextField(max_length=255)
     dateSent = models.DateTimeField(default = timezone.now)
     recipient = models.ForeignKey(User, on_delete=models.CASCADE)
+    readFlag = models.BooleanField(default = False)
 
     def __str__(self):
         return str(self.messageText)
+
+    class Meta:
+        ordering = ['-dateSent']
 
 
 
@@ -44,19 +45,17 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     posted_in = models.ForeignKey(Channel, on_delete=models.CASCADE)
-    topics  = models.ManyToManyField(Topic)
+    # topics  = models.ManyToManyField(Topic)
 
     class Meta:
         unique_together = ('title', 'created_by', 'posted_in')
+        ordering = ['datePosted']
 
     def notify(self):
-        topics = self.topics.all()
-        for topic in  topics:
-            subscribers = topic.subscribers.all()
-            for subsciber in subscribers:
-                m = Message(messageText = 'TEST MESSAGE',recipient = subsciber)
-                m.save()
-                
+        # topics = self.topics.all()
+        for subscriber in  self.posted_in.subscribers.all():
+            m = Message(messageText = 'New Post in channel: ' + self.posted_in, recipient = subscriber)
+            m.save()                
     
     def __str__(self):
         return str(self.title)
