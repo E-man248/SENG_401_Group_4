1ac1b89f4ded2d7d4533e73e83b1a7799ced5055
StayFrostea <59980526+StayFrostea@users.noreply.github.com>
2022-03-24 16:01:24 -0600

    Making the tests for Post/Channel

--- acafeed/channels/models.py
+++ acafeed/channels/models.py
@@ -7,36 +7,35 @@ from django.db import models
 from django.utils import timezone
 from users.models import User
 
+
 class PostTag(models.Model):
     tagName = models.CharField(max_length=32)
 
     def __str__(self):
         return str(self.tagName)
 
+
 class Channel(models.Model):
     name = models.CharField(max_length=255, unique=True)
     date_posted = models.DateTimeField(default=timezone.now)
-<<<<<<< Updated upstream
     subscribers = models.ManyToManyField(User)
 
     def subscribe(self, user):
         self.subscribers.add(user)
 
     def unsubscribe(self, user):
-        u = User.objects.get(userName = user)
+        u = User.objects.get(userName=user)
         self.topic_set.remove(u)
-
-=======
->>>>>>> Stashed changes
 
     def __str__(self):
         return str(self.name)
 
+
 class Message(models.Model):
     messageText = models.TextField(max_length=255)
-    dateSent = models.DateTimeField(default = timezone.now)
+    dateSent = models.DateTimeField(default=timezone.now)
     recipient = models.ForeignKey(User, on_delete=models.CASCADE)
-    readFlag = models.BooleanField(default = False)
+    readFlag = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.messageText)
@@ -51,6 +50,7 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     posted_in = models.ForeignKey(Channel, on_delete=models.CASCADE)
+
     # tags = models.ManyToManyField(PostTag)
 
     class Meta:
@@ -58,9 +58,9 @@ class Post(models.Model):
         ordering = ['date_posted']
 
     def notify(self):
-        for subscriber in  self.posted_in.subscribers.all():
-            m = Message(messageText = 'New Post in channel: ' + self.posted_in, recipient = subscriber)
-            m.save()                
-    
+        for subscriber in self.posted_in.subscribers.all():
+            m = Message(messageText='New Post in channel: ' + self.posted_in, recipient=subscriber)
+            m.save()
+
     def __str__(self):
         return str(self.title)
--- acafeed/channels/tests.py
+++ acafeed/channels/tests.py
@@ -1,33 +1,83 @@
 from django.test import TestCase
 from django.db import models
-from models import Channel
-from models import Post
+from .models import Channel
+from .models import Post
+from users.models import User
 
-
-# Create your tests here.
 
 class ChannelTestCase(TestCase):
 
     def setUp(self):
-
         Channel.objects.create(
             name="Homework",
         )
 
     def test_create_channel(self):
-        c = Channel.objects.get(name="SENG533")
-        self.assertEqual(c.name, "SENG533")
+        c = Channel.objects.get(name="Homework")
+        self.assertEqual(c.name, "Homework")
 
-class PostTestCase(TestCase):
+
+class PostChannelTestCase(TestCase):
 
     def setUp(self):
+        u = User(
+            name="Charles",
+            email="CharlesHasMail@gmail.com",
+            year=3,
+            userName="New_Guy",
+            password="1234abc",
+            major="Software Engineer",
+            school="University of Calgary",
+            blocked=False,
+        ).save()
+
+        c = Channel(name="test").save()
 
         Post.objects.create(
             title="Homework",
             content="Hello World!",
-            
-        )
+            created_by=User.objects.get(name="Charles"),
+            posted_in=Channel.objects.get(name="test"),
+        ).save()
 
     def test_create_post(self):
-        p = Post.objects.get(name="SENG533")
-        self.assertEqual(c.name, "SENG533")
+        p = Post.objects.get(title="Homework", )
+        self.assertEqual(p.content, "Hello World!")
+        self.assertEqual(p.posted_in.name, "test")
+
+
+class MessageTestCase(TestCase):
+
+    def setUp(self):
+        User.objects.create(
+            name="Charles",
+            email="CharlesHasMail@gmail.com",
+            year=3,
+            userName="New_Guy",
+            password="1234abc",
+            major="Software Engineer",
+            school="University of Calgary",
+            blocked=False,
+        )
+
+        Channel.objects.create(name="test")
+
+        Post.objects.create(
+            title="Homework",
+            content="Hello World!",
+            created_by=User.objects.get(name="Charles"),
+            posted_in=Channel.objects.get(name="test"),
+        )
+
+    def test_message_notify(self):
+        p = Post.objects.get(title="Homework")
+        c = Channel.objects.get(name="test")
+        u = User.objects.get(name="Charles")
+
+        c.subscribe(u)
+        p.notify()
+
+        messages = u.message_set.all()
+
+        self.assertEqual(messages.first.messageText, "New post in channel: " + c.name)
+
